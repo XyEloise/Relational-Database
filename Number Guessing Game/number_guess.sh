30935c4969356cbca6769c2f0da0f4a9c290f2d3
Eloise <96690906+XyEloise@users.noreply.github.com>
2023-10-02 21:31:35 -0600

    Update number_guess.sh
    should include the last correct guess as one guess also!

--- Number Guessing Game/number_guess.sh
+++ Number Guessing Game/number_guess.sh
@@ -3,10 +3,12 @@
 PSQL="psql --username=freecodecamp --dbname=number_guess_db -Atc"
 
 input_number(){
-  echo "Guess the secret number between 1 and 1000:"
+  #echo "Guess the secret number between 1 and 1000:"
   read NUMBER
   while [[ ! $NUMBER =~ ^[0-9]+$ ]]
   do 
+    number_of_guesses=$(($number_of_guesses+1))
+    #echo number_of_guesses is $number_of_guesses
     echo "That is not an integer, guess again:"
     read NUMBER
   done
@@ -26,8 +28,10 @@ then
   games_played=$($PSQL "SELECT games_played FROM info WHERE name='$NAME'")
   best_game=$($PSQL "SELECT best_game FROM info WHERE name='$NAME'")
   echo -e "Welcome back, $NAME! You have played $games_played games, and your best game took $best_game guesses."
+  echo "Guess the secret number between 1 and 1000:"
 else
   echo -e "Welcome, $NAME! It looks like this is your first time here."
+  echo "Guess the secret number between 1 and 1000:"
   NEW_INSERT=$($PSQL "INSERT INTO info(name,games_played) VALUES('$NAME',0)")
   games_played=0
   best_game=0
@@ -35,8 +39,8 @@ fi
 
 secret_number=$(( RANDOM%999+1 ))
 #echo secret_number=$secret_number
+number_of_guesses=0
 input_number
-number_of_guesses=1
 while [[ $secret_number != $NUMBER ]]
 do 
   if [[ $secret_number > $NUMBER ]]
@@ -46,11 +50,13 @@ do
     echo "It's higher than that, guess again:"
   fi
   number_of_guesses=$(($number_of_guesses+1))
+  #echo number_of_guesses is $number_of_guesses
   input_number
 done
 
 if [[ $secret_number == $NUMBER ]]
 then
+  number_of_guesses=$(($number_of_guesses+1))
   echo -e "You guessed it in $number_of_guesses tries. The secret number was $secret_number. Nice job!"
   UPDATE_INSERT=$($PSQL "UPDATE info SET games_played=$(($games_played+1))")
   #echo number_of_guesses is $number_of_guesses
